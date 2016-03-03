f309a5b23d0ad671580d098e3cb3e1cdaab23a4e
Erik-k <erik.knechtel@gmail.com>
2016-03-03 20:11:07 +0000

    Not sure why it cant detect zenith hits

--- cosmicray/cosmicray_detector.py
+++ cosmicray/cosmicray_detector.py
@@ -1,34 +1,44 @@
-# To do: make sure the NAND hits take precedence over hits from tube1 
+# To do: make sure the NOR hits take precedence over hits from tube1 
 # and tube2 which will occur at the same time.
 
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
 GeigerTube1 = "P9_41"
-#GeigerTube2 = "P9_42"
-#NAND = "P9_43"
+GeigerTube2 = "P9_42"
+NOR = "P9_40"
 
 GPIO.setup(GeigerTube1, GPIO.IN)
-#GPIO.setup(GeigerTube2, GPIO.IN)
-#GPIO.setup(NAND, GPIO.IN)
+GPIO.setup(GeigerTube2, GPIO.IN)
+GPIO.setup(NOR, GPIO.IN)
 
 Tube1Hits = 0
 Tube2Hits = 0
 ZenithHits = 0
 
-def recordEvent(tube):
-	global Tube1Hits, Tube2Hits
-	print "Recording event on " + tube
-#	if tube == 1:
-#		Tube1Hits += 1
-#	elif tube == 2:
-#		Tube2Hits += 1
-#	else:
-#		ZenithHits += 1
+# Need to have an argument inside the parantheses even if it isnt used
+def recordEvent1(pin):
+	global Tube1Hits
+	print "Recording event on tube 1"
+	Tube1Hits += 1
 
-GPIO.add_event_detect(GeigerTube1, GPIO.FALLING, callback=recordEvent)
-#GPIO.add_event_detect(GeigerTube2, GPIO.FALLING, callback=recordEvent)
-#GPIO.add_event_detect(NAND, GPIO.FALLING, callback=recordEvent)
+def recordEvent2(pin):
+	global Tube2Hits
+	print "Recording event on tube 2"
+	Tube2Hits += 1
+
+def recordZenithEvent(pin):
+	global ZenithHits
+	print "Cosmic Ray!"
+	ZenithHits += 1
+
+GPIO.add_event_detect(NOR, GPIO.FALLING, callback=recordZenithEvent)
+GPIO.add_event_detect(GeigerTube1, GPIO.FALLING, callback=recordEvent1)
+GPIO.add_event_detect(GeigerTube2, GPIO.FALLING, callback=recordEvent2)
 while True:
-	time.sleep(1000)
+	time.sleep(10)
+	print "The hits so far:"
+	print "Tube 1: ", Tube1Hits
+	print "Tube 2: ", Tube2Hits
+	print "Cosmic Rays: ", ZenithHits
 print "Program ended."
